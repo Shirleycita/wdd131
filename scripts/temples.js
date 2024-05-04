e255bfabe3dac46aab0d9079d68ffb8f1988ce8f
Shirleycita <smvycwh6@hotmail.com>
2024-05-04 02:19:09 -0700

    syntax changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,4 @@ document.addEventListener("DOMContentLoaded", function() {
  
     var lastModified = new Date(document.lastModified);
     document.getElementById("lastModified").textContent = lastModified.toLocaleDateString();
+});
